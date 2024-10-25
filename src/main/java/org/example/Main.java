3cad2dd8cf411d3b69093a9273b3c348385cc8f8
Op-Bshaar <basharabdalla5@gmail.com>
2024-10-25 11:47:41 +0300

    ls,exit,mkdir,rm

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,44 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("Welcome");
+        System.out.println("please enter command :)");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("> ");
+
+        while (true) {
+            String commandIn = scanner.nextLine().trim();
+            String[] parts = commandIn.split(" ");
+            String cmd = parts[0];
+            String[] argsArray = parts.length > 1 ? new String[]{parts[1]} : new String[0];
+            Command command;
+
+            switch (cmd) {
+                case "mkdir":
+                    command = new MkdirCommand();
+                    System.out.println(command.run(argsArray));
+                    break;
+                case "rm":
+                    command = new RmCommand();
+                    System.out.println(command.run(argsArray));
+                    break;
+                case "ls":
+                    command = new LsCommand();
+                    System.out.println(command.run(argsArray));
+                    break;
+                case "exit":
+                    System.out.println("Exiting..");
+                    scanner.close();
+                    return;
+                default:
+                    System.out.println("Command " + cmd + " not found.");
+            }
+            System.out.print("> ");
         }
     }
 }
