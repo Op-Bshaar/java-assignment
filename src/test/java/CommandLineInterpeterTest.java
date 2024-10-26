c990f398687125a907f47fb9f5f15e5a84d4bad7
Op-Bshaar <basharabdalla5@gmail.com>
2024-10-26 20:54:53 +0300

    طلعت عينا

--- src/test/java/CommandLineInterpeterTest.java
+++ src/test/java/CommandLineInterpeterTest.java
@@ -2,6 +2,9 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.example.CommandLineInterpeter;
+
+import java.nio.file.Paths;
+
 public class CommandLineInterpeterTest {
     @Test
     public  void testPwd(){
@@ -12,6 +15,28 @@ public class CommandLineInterpeterTest {
     }
     @Test
     public void testCat(){
-        
+
+    }
+
+   @Test
+    public void testmkdir(){
+       CommandLineInterpeter cmd = new CommandLineInterpeter();
+       String result = cmd.MkdirCommand(new String[] {"bashar"});
+       assertEquals("Directory created: " + Paths.get("bashar").toAbsolutePath(), result);
+    }
+ @Test
+   public void testLs(){
+        CommandLineInterpeter cmd = new CommandLineInterpeter();
+        String result = cmd.MkdirCommand(new String[] {"bashar"});
+  }
+ @Test
+    public void TestRm(){
+        CommandLineInterpeter cmd = new CommandLineInterpeter();
+        String result = cmd.RmCommand(new String[] {"bashar"});
+        assertEquals("Directory deleted: " + Paths.get("bashar").toAbsolutePath(), result);
+    }
+  @Test
+    public  void TestExit(){
+
     }
 }
